0553ea1aeac20e3e6f0634e251f46c250154abd1
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-08-15 13:53:16 +0330

    Save logic completed and refactored

--- HosseinNazarieh/modules/save.py
+++ HosseinNazarieh/modules/save.py
@@ -1,6 +1,20 @@
 def saveParagraph():
     firstParagraph = input()
     secondParagraph = input()
+    cntr = 40
+    while cntr < len(firstParagraph):
+        leftPart = firstParagraph[:cntr] + '\n'
+        if cntr + 1 < len(firstParagraph):
+            rightPart = firstParagraph[cntr:]
+        firstParagraph = leftPart + rightPart
+        cntr += cntr
+    cntr = 40
+    while cntr < len(secondParagraph):
+        leftPart = secondParagraph[:cntr] + '\n'
+        if cntr + 1 < len(secondParagraph):
+            rightPart = secondParagraph[cntr:]
+        secondParagraph = leftPart + rightPart
+        cntr += cntr
     with open('paragraphs.txt', 'w') as f:
         f.write(firstParagraph)
         f.write(secondParagraph)
